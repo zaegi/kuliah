30dd7f7725b54fd5e286811b25c5e915fc8a0239
zaegi <dezzaegitanta1@stundent.esaunggul.ac.id>
2021-11-02 14:28:52 +0700

    tugas6

--- pert6/source1.cpp
+++ pert6/source1.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 void cetakUmur(int umur){
- cout<< "Halo, Umur kamu adalah" <<umur <<" tahun";
+ cout<< "Halo, Umur kamu adalah " <<umur <<" tahun";
 }
 int main(){
- int angka=20;
+ int angka;
+
+ cout<<"masukan umurmu : ";
+ cin>>angka;
  cetakUmur(angka);
  getchar();
  return 0;
